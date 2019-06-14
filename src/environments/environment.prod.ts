b355b33b2395d81e293e4c81f6e31f09a57bca30
landrzejewski <landrzejewski.poczta@gmail.com>
2019-06-14 15:30:23 +0200

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyC2FVmvHoJAOXVfLqgZZwSYWCUv5lSPDQ0",
-    authDomain: "chat-e66c5.firebaseapp.com",
-    databaseURL: "https://chat-e66c5.firebaseio.com",
-    projectId: "chat-e66c5",
-    storageBucket: "chat-e66c5.appspot.com",
-    messagingSenderId: "1000183937264",
-    appId: "1:1000183937264:web:8abd113aaf1f1a25"
+
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC2FVmvHoJAOXVfLqgZZwSYWCUv5lSPDQ0",
-    authDomain: "chat-e66c5.firebaseapp.com",
-    databaseURL: "https://chat-e66c5.firebaseio.com",
-    projectId: "chat-e66c5",
-    storageBucket: "chat-e66c5.appspot.com",
-    messagingSenderId: "1000183937264",
-    appId: "1:1000183937264:web:8abd113aaf1f1a25"
+
   }
 };
 
